ddaa0cc4a7a3f26dee60b1c43ae5526a890f0fbd
tae.y <0211ilyoil@gmail.com>
2023-10-05 08:00:01 +0900

    예외 필터 추가

---
+++ src/main/java/petition/petition/global/error/GlobalExceptionFilter.java
@@ -0,0 +1,42 @@
+package petition.petition.global.error;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.filter.OncePerRequestFilter;
+import petition.petition.global.error.exception.BusinessException;
+import petition.petition.global.error.exception.ErrorCode;
+
+import javax.servlet.FilterChain;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@RequiredArgsConstructor
+public class GlobalExceptionFilter extends OncePerRequestFilter {
+
+    private final ObjectMapper objectMapper;
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            HttpServletResponse response,
+            FilterChain filterChain
+    ) throws IOException {
+
+        try {
+            filterChain.doFilter(request, response);
+        } catch (BusinessException e){
+            ErrorCode errorCode = e.getErrorCode();
+            writeErrorResponse(response, errorCode.getStatusCode(), ErrorResponse.of(errorCode, errorCode.getMessage()));
+        } catch (Exception e){
+            writeErrorResponse(response, response.getStatus(), ErrorResponse.of(response.getStatus(), e.getMessage()));
+        }
+    }
+
+    private void writeErrorResponse(HttpServletResponse response, int statusCode, ErrorResponse errorResponse) throws IOException {
+        response.setStatus(statusCode);
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        objectMapper.writeValue(response.getWriter(), errorResponse);
+    }
+}
