717e422dcdb622eb8aea281a078786a5d560fb66
tae.y <0211ilyoil@gmail.com>
2023-10-09 14:30:59 +0900

    dto 추가

--- src/main/java/petition/petition/domain/user/presentation/dto/request/CheckDuplicationRequest.java
+++ src/main/java/petition/petition/domain/user/presentation/dto/request/CheckDuplicationRequest.java
@@ -3,10 +3,13 @@ package petition.petition.domain.user.presentation.dto.request;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.Size;
+
 @NoArgsConstructor
 @Getter
 public class CheckDuplicationRequest {
 
+    @Size(min = 5, max = 15, message = "아이디는 최소 5자, 최대 15자 입니다.")
     private String accountId;
 
 }
--- src/main/java/petition/petition/domain/user/presentation/dto/response/MyInfoResponse.java
+++ src/main/java/petition/petition/domain/user/presentation/dto/response/MyInfoResponse.java
@@ -1,11 +1,8 @@
 package petition.petition.domain.user.presentation.dto.response;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import petition.petition.domain.user.domain.type.Role;
-
-import javax.persistence.Access;
 
 @Builder
 @Getter
