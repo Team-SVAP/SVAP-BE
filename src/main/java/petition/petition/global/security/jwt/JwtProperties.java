30a3c93a4fbc82c2e9ddb600e6c19e85cf29a328
tae.y <0211ilyoil@gmail.com>
2023-11-03 13:17:23 +0900

    리팩토링

--- src/main/java/petition/petition/global/security/jwt/JwtProperties.java
+++ src/main/java/petition/petition/global/security/jwt/JwtProperties.java
@@ -1,20 +1,32 @@
 package petition.petition.global.security.jwt;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
 
-@AllArgsConstructor
+import java.util.Base64;
+
 @Getter
 @ConstructorBinding
-@ConfigurationProperties(prefix = "auth.jwt")
+@ConfigurationProperties(prefix = "jwt")
 public class JwtProperties {
 
     private final String secretKey;
-    private final Long accessExp = 604800L;//엑세스 토큰의 만료시간은 일주일
-    private final Long refreshExp=604800L;//리프레쉬 토큰의 만료시간은 1주일
+
+    private final Long accessExp;
+
+    private final Long refreshExp;
+
     private final String header;
+
     private final String prefix;
+
+    public JwtProperties(String secretKey, Long accessExp, Long refreshExp, String header, String prefix) {
+        this.secretKey = Base64.getEncoder().encodeToString(secretKey.getBytes());
+        this.accessExp = accessExp;
+        this.refreshExp = refreshExp;
+        this.header = header;
+        this.prefix = prefix;
+    }
 
 }
