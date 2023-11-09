af58f0f3c59ae96cfd5bbb8d93661775ac9d7580
tae.y <0211ilyoil@gmail.com>
2023-11-09 10:26:52 +0900

    🧩:: CustomRepository, RepositoryImpl 추가

---
+++ src/main/java/petition/petition/domain/petition/domain/repository/PetitionCustomRepository.java
@@ -0,0 +1,15 @@
+package petition.petition.domain.petition.domain.repository;
+
+import petition.petition.domain.petition.domain.Petition;
+import petition.petition.domain.petition.domain.types.AccessTypes;
+import petition.petition.domain.petition.domain.types.Types;
+import petition.petition.domain.user.domain.User;
+
+import java.util.List;
+
+public interface PetitionCustomRepository {
+
+    List<Petition> queryPetitionByTypesAndAccessTypes(Types types, AccessTypes accessTypes);
+
+    List<Petition> queryPetitionByAccessTypes(AccessTypes accessTypes);
+}
