d738d634a3f2f947e7fb906091de82d4607b581b
tae.y <0211ilyoil@gmail.com>
2023-11-30 19:17:25 +0900

    🧩:: WebConfig 추가

---
+++ src/main/java/petition/petition/global/config/WebConfig.java
@@ -0,0 +1,22 @@
+package petition.petition.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000",
+                        "http://lcoalhost:3001",
+                        "http://localhost:4000"
+                )
+                .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "HEAD")
+                .allowedHeaders("*");
+    }
+}
