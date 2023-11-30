fe1bcdbec9fd3f98829e9cdeea8153f3aa5eedeb
tae.y <0211ilyoil@gmail.com>
2023-11-30 19:18:15 +0900

    🧩:: Cors 메서드 삭제

--- src/main/java/petition/petition/global/config/SecurityConfig.java
+++ src/main/java/petition/petition/global/config/SecurityConfig.java
@@ -10,15 +10,10 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import petition.petition.global.error.GlobalExceptionFilter;
 import petition.petition.global.security.jwt.JwtReissueUtil;
 import petition.petition.global.security.jwt.JwtTokenFilter;
 import petition.petition.global.security.jwt.JwtTokenProvider;
-
-import java.util.Arrays;
 
 
 @RequiredArgsConstructor
@@ -55,19 +50,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .addFilterBefore(new JwtTokenFilter(jwtTokenProvider, jwtReissueUtil), UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(new GlobalExceptionFilter(objectMapper), JwtTokenFilter.class);
 
-    }
-
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-        CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(Arrays.asList("*"));
-        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE")); // HTTP 메서드 허용
-        configuration.setAllowCredentials(true);
-        configuration.addAllowedHeader("*");
-
-        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        source.registerCorsConfiguration("/**", configuration);
-        return source;
     }
 
     @Bean
