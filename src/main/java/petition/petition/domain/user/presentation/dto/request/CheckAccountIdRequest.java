6ab93ddda1120bbd494af801a9631a4db5dd426d
tae.y <0211ilyoil@gmail.com>
2023-12-19 15:51:55 +0900

    🛠 :: 리팩토링

--- src/main/java/petition/petition/domain/user/presentation/dto/request/CheckAccountIdRequest.java
+++ src/main/java/petition/petition/domain/user/presentation/dto/request/CheckAccountIdRequest.java
@@ -1,12 +1,13 @@
 package petition.petition.domain.user.presentation.dto.request;
 
+import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.Size;
 
-@NoArgsConstructor
 @Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class CheckAccountIdRequest {
 
     @Size(min = 8, max = 30, message = "아이디는 최소 8자, 최대 30자 입니다.")
